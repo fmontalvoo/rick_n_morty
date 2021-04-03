be602264d950b39618907f14b29249f74f76feba
Frank Montalvo Ochoa <fmontalvoo@ups.edu.ec>
2021-04-03 17:36:27 -0500

    Se obtiene la informacion del API y se muestra en el template del home.
    Signed-off-by: Frank Montalvo Ochoa <fmontalvoo@ups.edu.ec>

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -20,9 +20,8 @@ const router = async () => {
 
     let hash = getHash();
     let route = await resolveRoutes(hash);
-    console.log(route);
+
     let render = routes[route] ? routes[route] : Error404;
-    console.log(render);
 
     content.innerHTML = await render();
 }
